a9e80fadae4d658714e962ce4007ae2c5378a427
Gaellugo <gaellugo99@hotmail.com>
2022-11-08 20:30:49 -0800

    inicializando servidor

---
+++ src/app/prueba/prueba.1component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector:'prueba',
+    template:'./prueba.component.html',
+    styles: ['./prueba.component.css'],
+})
+
+export class prueba {
+    colores=['rojo,verde,azul']
+}
